e5c77fb7d4a8493d96b74fccb9568d0ebf7d9f2d
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-04-27 10:30:38 -0400

    payments-add ref ID

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -24,6 +24,7 @@ export async function submitPayment(
     const result = await paymentsApi.createPayment({
       idempotencyKey: randomUUID(),
       sourceId,
+      referenceId: "4elkql4m3b5gnkfsgud0abpl9m",
       billingAddress: {
         addressLine1: billingDetails.addressLine1,
         addressLine2: billingDetails.addressLine2 || "",
