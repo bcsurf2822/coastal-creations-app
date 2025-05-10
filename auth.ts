6aef3279d4918f07970e98e24e29ea0946b1eb29
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-05-10 07:42:34 -0400

    fix api auth route

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import Google from "next-auth/providers/google"
+import Google from "next-auth/providers/google";
 import clientPromise from "./lib/mongodb";
 
-
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error("Missing Google OAuth credentials");
+}
 
 const config = {
   providers: [
@@ -15,4 +17,4 @@ const config = {
   adapter: MongoDBAdapter(clientPromise),
 };
 
-export const { handlers, signIn, signOut, auth } = NextAuth(config)
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
