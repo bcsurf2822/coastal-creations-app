f16145ef402e17edbed732636776d2725fb1946c
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-05-17 09:09:01 -0400

    protected route

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import EventContainer from "@/components/dashboard/EventContainer";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/auth";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/admin");
+  }
+
   return (
     // Applying Tailwind classes for the main flex layout
     <div className="flex min-h-screen bg-gray-100">
