2356c06d60857aafa477232ec6cc54830810f42b
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-06-08 14:48:15 -0400

    fix cal orientation

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,8 +5,10 @@ import React from "react";
 
 export default function CalendarPage() {
   return (
-    <main className="min-h-screen py-12">
-      <h1 className="text-center text-2xl font-bold mb-6">Events Calendar</h1>
+    <main className="min-h-screen py-12 pb-20">
+      <h1 className="text-center text-2xl font-bold mb-6 font-comic-neue">
+        Events Calendar
+      </h1>
       <NewCalendar />
     </main>
   );
