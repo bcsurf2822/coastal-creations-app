b103887966973fee31c0bbb675eac1e9a1e80f30
Benjamin Corbett <111916951+bcsurf2822@users.noreply.github.com>
2025-10-10 10:30:37 -0400

    rem classes

--- app/classes/private-events/page.tsx
+++
@@ -1,5 +0,0 @@
-import PrivateEvents from "@/components/classes/privateEvents/PrivateEvents";
-
-export default function PrivateEventsPage() {
-  return <PrivateEvents />;
-}
--- app/classes/summer-camps/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SummerCamps from "../../../components/classes/summer-camps/SummerCamps";
-
-export default function SummerCampsPage() {
-  return <SummerCamps />;
-}
